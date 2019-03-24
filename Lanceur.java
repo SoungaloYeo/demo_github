ebcd5cbc325ea64bd5727df565713054a002905f
soungaloyeo <soungaloyeo@gmail.com>
2019-03-24 00:52:58 +0000

    Modification Lanceur en y creant un nouvel etudiant

--- Lanceur.java
+++ Lanceur.java
@@ -1,8 +1,9 @@
 public class Lanceur{
-	
+	Etudiant etudiant = null;	
 
 	public static void main(String... args){
 		// dire bonjour
+		etudiant = new Etudiant("YEO");
 		System.out.print("Hello world");
 	}
 }
